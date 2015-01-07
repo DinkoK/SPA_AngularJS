72ef178da5b35a46dbe806f4637129ab852ab0ff
DinkoK <dinko.krastev89@gmail.com>
2015-01-07 14:55:29 +0200

    Frist STEP _ WORKING!!!!!!

--- Angular_Project/Angular_Project/app/js/app.js
+++ Angular_Project/Angular_Project/app/js/app.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-var app = angular.module('app', ['ngRoute', 'ngResource','angular-loading-bar']);
+var app = angular.module('app', ['ngRoute', 'ngResource', 'angular-loading-bar', 'ui.bootstrap.pagination']);
 
 app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net/api/ads');
 app.constant('pageSize', 5);
