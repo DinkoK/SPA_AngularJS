ecc013deadeaf5bbad3ea0773c78e76d54ec885b
DinkoK <dinko.krastev89@gmail.com>
2015-01-08 14:38:13 +0200

    Login Done

--- Angular_Project/Angular_Project/app/js/controllers/AppController.js
+++ Angular_Project/Angular_Project/app/js/controllers/AppController.js
@@ -1,5 +1,10 @@
 ﻿'use strict';
-app.controller('AppController', function ($scope, authService) {
+app.controller('AppController', function ($scope, authService, notifyService) {
     $scope.authService = authService;
+    $scope.logout = function () {
+        authService.logout();
+        notifyService.showInfo('Logout successful');        
+        $location.path("/");
+    };
 
 })
